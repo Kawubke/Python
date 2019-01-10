c10d1ec68c1da2f132ac32390ef5fba0797ed94b
Kawubke <46402851+Kawubke@users.noreply.github.com>
2019-01-10 21:20:50 +0100

    Update Ritter_Kunibert.py
    Drei Anführungszeichen hinzugefügt für bessere Lesbarkeit

--- Ritter_Kunibert.py
+++ Ritter_Kunibert.py
@@ -2,7 +2,10 @@ import random
 import time
 
 def displayIntro():
-    print('"Du bist in einer Welt gefangen als Ritter Kunibert. Der einzige Weg zu überleben ist es, zwischen zwei Höhleneingängen zu wählen. In einer wohnt ein hungriger Löwe in der anderen ein süßes Kätchen. Der Löwe wird dich sofort verschlingen; die Katze will nur kuscheln."')
+    print('''Du bist in einer Welt gefangen als Ritter Kunibert.
+Der einzige Weg zu überleben ist es, zwischen zwei Höhleneingängen zu wählen.
+In einer wohnt ein hungriger Löwe in der anderen ein süßes Kätchen.
+Der Löwe wird dich sofort verschlingen; die Katze will nur kuscheln.''')
     print()
 
 def chooseCave():
@@ -18,16 +21,19 @@ def checkCave(chosenCave):
     time.sleep(2)
     print('Es ist furchtbar dunkel....')
     time.sleep(2)
-    print('Etwas wuscheliges springt aus dem dunkeln auf dich zu. Du kannst auf den ersten Blick nicht erkennen was es ist doch dann stehst du vor...')
+    print('''Etwas wuscheliges springt aus dem dunkeln auf dich zu.
+Du kannst auf den ersten Blick nicht erkennen was es ist doch dann stehst du vor...''')
     print()
     time.sleep (2)
 
     friendlyCave = random.randint(1, 2)
 
     if chosenCave == str(friendlyCave):
-        print('Es kuschelt und schnurrt! Das kann nur das Kätchen sein. welch ein Glück!')
+        print('''Es kuschelt und schnurrt! Das kann nur das Kätchen sein.
+Welch ein Glück!''')
     else:
-        print('Du kannst noch die Zähne im dunkeln aufblitzen sehen... dann ist alles dunkel. Es war der Löwe :(')
+        print('''Du kannst noch die Zähne im dunkeln aufblitzen sehen... dann ist alles dunkel.
+Es war der Löwe''')
 
 playAgain = 'Ja'
 while playAgain == 'Ja' or playAgain == 'j':
